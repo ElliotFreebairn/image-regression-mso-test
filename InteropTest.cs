aac6acc95fd2f0449aed50b67fd584a0e5830142
Pranam Lashkari <lpranam@collabora.com>
2023-04-06 04:26:30 +0530

    Add project files.

---
+++ InteropTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mso_test
+{
+    internal class InteropTest
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
