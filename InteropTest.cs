81f2f32a5f27dde7efdf0a84de0f1f6447634d6a
Pranam Lashkari <lpranam@collabora.com>
2023-04-07 05:35:58 +0530

    introduced MSO file opening tests

--- InteropTest.cs
+++ InteropTest.cs
@@ -3,14 +3,90 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using word = Microsoft.Office.Interop.Word;
+using excel = Microsoft.Office.Interop.Excel;
+using powerPoint = Microsoft.Office.Interop.PowerPoint;
+using System.Runtime.InteropServices;
 
 namespace mso_test
 {
     internal class InteropTest
     {
+        public static word.Application wordApp = new word.Application();
+        public static excel.Application excelApp = new excel.Application();
+        public static powerPoint.Application powerPointApp = new powerPoint.Application();
+
         private static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+            wordApp.Quit();
+            excelApp.Quit();
+            powerPointApp.Quit();
+        }
+
+        private static (bool, string) TestWordDoc(string fileName)
+        {
+            word.Document doc = null;
+            bool testResult = true;
+            string errorMessage = "";
+
+            try
+            {
+                doc = wordApp.Documents.OpenNoRepairDialog(fileName, ReadOnly: true, Visible: false);
+            }
+            catch (COMException e)
+            {
+                testResult = false;
+                errorMessage = e.Message;
+            }
+            if (doc != null)
+                doc.Close();
+
+            return (testResult, errorMessage);
+        }
+
+        private static (bool, string) TestExcelWorkbook(string fileName)
+        {
+            excel.Workbook wb = null;
+            bool testResult = true;
+            string errorMessage = "";
+
+            try
+            {
+                wb = excelApp.Workbooks.Open(fileName, ReadOnly: true);
+            }
+            catch (COMException e)
+            {
+                testResult = false;
+                errorMessage = e.Message;
+            }
+
+            if (wb != null)
+                wb.Close();
+
+            return (testResult, errorMessage);
+        }
+
+        private static (bool, string) TestPowerPointPresentation(string fileName)
+        {
+            powerPoint.Presentation presentation = null;
+            bool testResult = true;
+            string errorMessage = "";
+
+            try
+            {
+                presentation = powerPointApp.Presentations.Open(fileName, ReadOnly: Microsoft.Office.Core.MsoTriState.msoTrue, WithWindow: Microsoft.Office.Core.MsoTriState.msoFalse);
+            }
+            catch (COMException e)
+            {
+                testResult = false;
+                errorMessage = e.Message;
+            }
+
+            if (presentation != null)
+                presentation.Close();
+
+            return (testResult, errorMessage);
         }
     }
 }
