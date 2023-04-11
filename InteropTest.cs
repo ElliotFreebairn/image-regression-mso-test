0295cc4b3faa2bd1af139786d9e5901a508e8875
Pranam Lashkari <lpranam@collabora.com>
2023-04-11 19:24:20 +0530

    Download bug list which contains attachments of MSO files or LibreOffice files

--- InteropTest.cs
+++ InteropTest.cs
@@ -7,6 +7,7 @@ using word = Microsoft.Office.Interop.Word;
 using excel = Microsoft.Office.Interop.Excel;
 using powerPoint = Microsoft.Office.Interop.PowerPoint;
 using System.Runtime.InteropServices;
+using System.Net.Http;
 
 namespace mso_test
 {
@@ -15,6 +16,13 @@ namespace mso_test
         public static word.Application wordApp = new word.Application();
         public static excel.Application excelApp = new excel.Application();
         public static powerPoint.Application powerPointApp = new powerPoint.Application();
+
+        public static HttpClient client = new HttpClient();
+
+        static InteropTest()
+        {
+            client.BaseAddress = new Uri("https://bugs.documentfoundation.org");
+        }
 
         private static void Main(string[] args)
         {
@@ -87,6 +95,29 @@ namespace mso_test
                 presentation.Close();
 
             return (testResult, errorMessage);
+        }
+
+        private static async void downloadBugList()
+        {
+            // search all the bugs which has MSO or libreoffice file attachments
+            string buglistParameters = "/buglist.cgi" +
+                "?f1=attachments.mimetype&f10=attachments.mimetype&f11=attachments.mimetype&f2=attachments.mimetype" +
+                "&f3=attachments.mimetype&f4=attachments.mimetype&f5=attachments.mimetype&f6=attachments.mimetype" +
+                "&f7=attachments.mimetype&f8=attachments.mimetype&f9=attachments.mimetype" +
+                "&j_top=OR&o1=equals&o10=equals&o11=equals&o2=equals&o3=equals&o4=equals&o5=equals&o6=equals&o7=equals&o8=equals&o9=equals" +
+                "&query_format=advanced&limit=0&ctype=csv&human=1&v1=application%2Fexcel&v10=application%2Fvnd.ms-powerpoint" +
+                "&v11=application%2Fvnd.openxmlformats-officedocument.presentationml.presentation&v2=application%2Fmsword" +
+                "&v3=application%2Fpowerpoint&v4=application%2Fvnd.oasis.opendocument.text&v5=application%2Fvnd.oasis.opendocument.presentation" +
+                "&v6=application%2Fvnd.oasis.opendocument.spreadsheet&v7=application%2Fvnd.openxmlformats-officedocument.wordprocessingml.document" +
+                "&v8=application%2Fvnd.ms-excel&v9=application%2Fvnd.openxmlformats-officedocument.spreadsheetml.sheet";
+
+            using (var respose = client.GetAsync(buglistParameters).Result.Content)
+            {
+                using (var fileStream = new FileStream("buglist.csv", FileMode.Create))
+                {
+                    await respose.CopyToAsync(fileStream);
+                }
+            }
         }
     }
 }
