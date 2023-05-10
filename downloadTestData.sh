445c30aa247b626c45e395e2b4bb8f34ea5be498
Pranam Lashkari <lpranam@collabora.com>
2023-05-10 15:04:17 +0700

    add compress flag to download from rsync

--- downloadTestData.sh
+++ downloadTestData.sh
@@ -1,23 +1,23 @@
-/usr/bin/rsync --progress -r -v -e /usr/bin/ssh crashtest.tdf:/srv/crashtestdata/files/bugtrackers/doc/ download
-/usr/bin/rsync --progress -r -v -e /usr/bin/ssh crashtest.tdf:/srv/crashtestdata/files/bugtrackers/docx/ download
-/usr/bin/rsync --progress -r -v -e /usr/bin/ssh crashtest.tdf:/srv/crashtestdata/files/bugtrackers/odt/ download
+/usr/bin/rsync --progress -r -v -z -e /usr/bin/ssh crashtest.tdf:/srv/crashtestdata/files/bugtrackers/doc download
+/usr/bin/rsync --progress -r -v -z -e /usr/bin/ssh crashtest.tdf:/srv/crashtestdata/files/bugtrackers/docx download
+/usr/bin/rsync --progress -r -v -z -e /usr/bin/ssh crashtest.tdf:/srv/crashtestdata/files/bugtrackers/odt download
 
-/usr/bin/rsync --progress -r -v -e /usr/bin/ssh crashtest.tdf:/srv/crashtestdata/files/forums/doc/ download
-/usr/bin/rsync --progress -r -v -e /usr/bin/ssh crashtest.tdf:/srv/crashtestdata/files/forums/docx/ download
-/usr/bin/rsync --progress -r -v -e /usr/bin/ssh crashtest.tdf:/srv/crashtestdata/files/forums/odt/ download
+/usr/bin/rsync --progress -r -v -z -e /usr/bin/ssh crashtest.tdf:/srv/crashtestdata/files/forums/doc download
+/usr/bin/rsync --progress -r -v -z -e /usr/bin/ssh crashtest.tdf:/srv/crashtestdata/files/forums/docx download
+/usr/bin/rsync --progress -r -v -z -e /usr/bin/ssh crashtest.tdf:/srv/crashtestdata/files/forums/odt download
 
-/usr/bin/rsync --progress -r -v -e /usr/bin/ssh crashtest.tdf:/srv/crashtestdata/files/bugtrackers/xls/ download
-/usr/bin/rsync --progress -r -v -e /usr/bin/ssh crashtest.tdf:/srv/crashtestdata/files/bugtrackers/xlsx/ download
-/usr/bin/rsync --progress -r -v -e /usr/bin/ssh crashtest.tdf:/srv/crashtestdata/files/bugtrackers/ods/ download
+/usr/bin/rsync --progress -r -v -z -e /usr/bin/ssh crashtest.tdf:/srv/crashtestdata/files/bugtrackers/xls download
+/usr/bin/rsync --progress -r -v -z -e /usr/bin/ssh crashtest.tdf:/srv/crashtestdata/files/bugtrackers/xlsx download
+/usr/bin/rsync --progress -r -v -z -e /usr/bin/ssh crashtest.tdf:/srv/crashtestdata/files/bugtrackers/ods download
 
-/usr/bin/rsync --progress -r -v -e /usr/bin/ssh crashtest.tdf:/srv/crashtestdata/files/forums/xls/ download
-/usr/bin/rsync --progress -r -v -e /usr/bin/ssh crashtest.tdf:/srv/crashtestdata/files/forums/xlsx/ download
-/usr/bin/rsync --progress -r -v -e /usr/bin/ssh crashtest.tdf:/srv/crashtestdata/files/forums/ods/ download
+/usr/bin/rsync --progress -r -v -z -e /usr/bin/ssh crashtest.tdf:/srv/crashtestdata/files/forums/xls download
+/usr/bin/rsync --progress -r -v -z -e /usr/bin/ssh crashtest.tdf:/srv/crashtestdata/files/forums/xlsx download
+/usr/bin/rsync --progress -r -v -z -e /usr/bin/ssh crashtest.tdf:/srv/crashtestdata/files/forums/ods download
 
-/usr/bin/rsync --progress -r -v -e /usr/bin/ssh crashtest.tdf:/srv/crashtestdata/files/bugtrackers/ppt/ download
-/usr/bin/rsync --progress -r -v -e /usr/bin/ssh crashtest.tdf:/srv/crashtestdata/files/bugtrackers/pptx/ download
-/usr/bin/rsync --progress -r -v -e /usr/bin/ssh crashtest.tdf:/srv/crashtestdata/files/bugtrackers/odp/ download
+/usr/bin/rsync --progress -r -v -z -e /usr/bin/ssh crashtest.tdf:/srv/crashtestdata/files/bugtrackers/ppt download
+/usr/bin/rsync --progress -r -v -z -e /usr/bin/ssh crashtest.tdf:/srv/crashtestdata/files/bugtrackers/pptx download
+/usr/bin/rsync --progress -r -v -z -e /usr/bin/ssh crashtest.tdf:/srv/crashtestdata/files/bugtrackers/odp download
 
-/usr/bin/rsync --progress -r -v -e /usr/bin/ssh crashtest.tdf:/srv/crashtestdata/files/forums/ppt/ download
-/usr/bin/rsync --progress -r -v -e /usr/bin/ssh crashtest.tdf:/srv/crashtestdata/files/forums/pptx/ download
-/usr/bin/rsync --progress -r -v -e /usr/bin/ssh crashtest.tdf:/srv/crashtestdata/files/forums/odp/ download
+/usr/bin/rsync --progress -r -v -z -e /usr/bin/ssh crashtest.tdf:/srv/crashtestdata/files/forums/ppt download
+/usr/bin/rsync --progress -r -v -z -e /usr/bin/ssh crashtest.tdf:/srv/crashtestdata/files/forums/pptx download
+/usr/bin/rsync --progress -r -v -z -e /usr/bin/ssh crashtest.tdf:/srv/crashtestdata/files/forums/odp download
