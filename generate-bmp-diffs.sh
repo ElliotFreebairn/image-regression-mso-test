e5cf6eefb2affa6478cdc4971dc80dc352cd8e22
elliotfreebairn <elliotfreebairn1@gmail.com>
2025-07-08 11:48:52 +0100

    Edited the pdf to bmp conveter to make sure that a directory is passed as an argument and also that only 5 pages are converted from each pdf to limit memory usage. Created a script that will go through each input directory and compare a authoritative bmp and import bmp and will produce an bmp with the differences in output directory.

---
+++ generate-bmp-diffs.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# need to loop over individual subdirectory
+for DIRECTORY in input/*; do
+  dir_name=$(basename $DIRECTORY)
+  # loop over in each subdirectory just the auth pages and get their reciprical import page
+
+  declare -A auth_map
+  declare -A import_map
+
+  while IFS= read -r file; do
+    page=$(echo "$file" | grep -oP 'page-\K[0-9]+' | sed 's/^0*//') # gets the page number from the file
+    auth_map["$page"]="$file"
+  done < <(find "$DIRECTORY" -name "*auth*.bmp") # passes the find output as input to the while loop
+
+  while IFS= read -r file; do
+    page=$(echo "$file" | grep -oP 'page-\K[0-9]+' | sed 's/^0*//')
+    import_map["$page"]="$file"
+  done < <(find "$DIRECTORY" -name "*import*.bmp")
+
+  for page in $(printf "%s\n" "${!auth_map[@]}" | sort -n); do
+    auth_file="${auth_map[$page]}"
+    import_file="${import_map[$page]}"
+
+    if [[ -n "$import_file" ]]; then
+      echo "$auth_file ---- $import_file"
+      # add the script to compare the files
+    else
+      echo "Missing page for $page"
+    fi;
+  done;
+
+  unset auth_map
+  unset import_map
+done;
