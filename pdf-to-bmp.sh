99cf9627990e399c790ad26c9cbb350b3f76265c
elliotfreebairn <elliotfreebairn1@gmail.com>
2025-07-08 09:40:41 +0100

    Pdf-to-bmp script can convert pdf's from an image-dump
    Signed-off-by: Elliot Freebairn <elliot.freebairn@collabora.com>

--- pdf-to-bmp.sh
+++ pdf-to-bmp.sh
@@ -1,11 +1,26 @@
-filepath=$1
-filename=$(basename $filepath)
+dir_to_search=$1 # should be "some_directory/"
 
-mkdir input/$filename output/$filename
-pdftoppm $filepath input/$filename/output -png -r 300
+for DIRECTORY in $dir_to_search*; do
+    dir_name=$(basename $DIRECTORY)
+    mkdir input/$dir_name
 
-for img in input/$filename/*.png; do
-    basename=$(basename "$img" .png)
-    convert "$img" -define bmp:format=bmp4 -alpha on "input/$filename/${basename}.bmp"
-    rm $img
-done
+    files=$(find $DIRECTORY/ -not -name '*prev*' -name '*.pdf')
+    for FILE in $files; do
+        # convert to png first (loseless)
+        output_name=""
+        if [[ $FILE =~ "authoritative" ]]; then
+            output_name="auth"
+        else
+            output_name="import"
+        fi
+        pdftoppm $FILE input/$dir_name/$output_name-page -png -r 150 
+    done;
+
+    # convert the png's to bmp's (may be multiple png's based on page count)
+    for IMG in input/$dir_name/*; do
+        img_name=$(basename "$IMG" .png)
+        convert $IMG -define bmp::format=bmp4 -alpha on "input/$dir_name/$img_name.bmp"
+        rm $IMG
+    done;
+done;
+    
