0a45a998e20e7dcd5d9165226910d0687c03a98d
elliotfreebairn <elliotfreebairn1@gmail.com>
2025-07-10 16:00:10 +0100

    Added a flag to the program that allows the user to enable minor differences to be highlighted. These minor differences are usually around texts and edges but are still large enough not to be insignificant. I also changed the conversion process to convert at a higher dpi to png and scale down the png by a quarter to bmp. This is to help smooth out more minor differences cause by rendering quirks and usually text related issues.

--- src/pixelbasher.cpp
+++ src/pixelbasher.cpp
@@ -64,11 +64,10 @@ std::vector<bool> PixelBasher::blurEdgeMask(const BMP& bmp, const std::vector<bo
           }
       }
   }
-
   return blurred_mask;
 }
 
-void PixelBasher::compareToBMP(BMP& base, const BMP& imported) {
+void PixelBasher::compareToBMP(BMP& base, const BMP& imported, bool enable_minor_differences) {
   int32_t min_width = std::min(base.get_width(), imported.get_width());
   int32_t min_height = std::min(base.get_height(), imported.get_height());
   int32_t pixel_stride = 4;
@@ -97,11 +96,12 @@ void PixelBasher::compareToBMP(BMP& base, const BMP& imported) {
       std::vector<uint8_t> bgra = {base.get_data()[currentIndex], base.get_data()[currentIndex + 1], base.get_data()[currentIndex + 2], base.get_data()[currentIndex + 3]};
       // instead of just not highlighting any text differences, maybe pass into differs_from and set a higher threshold it has to pass and if so make the values yellow
       if (p1.differs_from(p2, intersection_mask[maskIndex])) {
-        // std::cout << "hello";
-        Colour colour = intersection_mask[maskIndex] ? Colour::YELLOW : Colour::RED;
-        bgra = colourPixel(colour);
+        if (intersection_mask[maskIndex] && enable_minor_differences) {
+          bgra = colourPixel(Colour::YELLOW);
+        } else {
+          bgra = colourPixel(Colour::RED);
+        }
       }
-
 
       for (int i = 0; i < pixel_stride; i++) {
         new_data[currentIndex + i] = bgra[i];
@@ -118,3 +118,4 @@ std::vector<uint8_t> PixelBasher::colourPixel(Colour colour) {
     return {0, 0, 255, 255};
   }
 }
+
