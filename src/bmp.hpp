cce2ecc0c2b2e218b2d813bc78fed4132ed07f5b
elliotfreebairn <elliotfreebairn1@gmail.com>
2025-07-16 09:30:58 +0100

    Adding MPLv2 licenses to the headers
    Signed-off-by: Elliot Freebairn <elliot.freebairn@collabora.com>

--- src/bmp.hpp
+++ src/bmp.hpp
@@ -1,3 +1,13 @@
+//
+//
+// Copyright the mso-test contributors
+//
+// SPDX-License-Identifier: MPL-2.0
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 #ifndef BMP_HPP
 #define BMP_HPP
 
