cbf09cf7da6ac80857ae6d75df7e82ee8a89fd3b
elliotfreebairn <elliotfreebairn1@gmail.com>
2025-07-16 12:51:20 +0100

    Changed the pdf conversion so that it skips out converting to png but goes straight to bmp, reducing the time covnerting exponentially. Also added/removed some whitespace in bmp.cpp
    Signed-off-by: Elliot Freebairn <elliot.freebairn@collabora.com>

--- src/bmp.cpp
+++ src/bmp.cpp
@@ -34,18 +34,18 @@ void BMP::read(const char* filename) {
   }
   if (info_header.height < 0) {
     throw std::runtime_error("The program can treat only BMP images with the origin in the bottom left corner!");
-}
-  
+  }
+
   input.seekg(file_header.offset_data, input.beg);
 
   info_header.size = sizeof(BMPInfoHeader) + sizeof(BMPColourHeader); // we know that its a 32-bit BMP
   file_header.offset_data = sizeof(BMPFileHeader) + sizeof(BMPInfoHeader) + sizeof(BMPColourHeader);
   file_header.file_size = file_header.offset_data;
-  
+
   row_stride = info_header.width * info_header.bit_count / 8;
   uint32_t alligned_stride = (row_stride + 3) & ~3; // This rounds up to the nearest multiple of 4
   size_t padding_size = alligned_stride - row_stride;
-  
+
   std::vector<uint8_t> padding(padding_size);
   data.resize(row_stride * info_header.height);
 
@@ -60,15 +60,14 @@ void BMP::read(const char* filename) {
   }
   file_header.file_size += data.size() + padding_size * info_header.height;
 }
-  
 
-  
+
 void BMP::write(const char* filename) {
   std::ofstream output{filename, std::ios_base::binary};
   if (!output) {
     throw std::runtime_error("Cannot open file for writing");
   }
-  
+
   // write the headers
   output.write((const char*)&file_header, sizeof(BMPFileHeader));
   output.write((const char*)&info_header, sizeof(BMPInfoHeader));
