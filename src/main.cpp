c2c6cdb43177fa695079c276a549b1c6f86fa4ff
elliotfreebairn <elliotfreebairn1@gmail.com>
2025-07-23 10:37:38 +0100

    Adding diffing for both LO  and MSO roundtripped files.
    Signed-off-by: Elliot Freebairn <elliot.freebairn@collabora.com>

--- src/main.cpp
+++ src/main.cpp
@@ -22,19 +22,24 @@ struct ParsedArguments {
 	std::string extension;
 	std::string import_dir;
 	std::string export_dir;
+	std::string import_compare_dir;
+	std::string export_compare_dir;
 	bool enable_minor_differences;
 	bool lo_previous;
+	bool ms_previous;
 	std::vector<BMP> authoritative_images;
 	std::vector<BMP> import_images;
 	std::vector<BMP> export_images;
 	std::vector<BMP> import_previous_images;
+	std::vector<BMP> export_previous_images;
 };
 
 ParsedArguments parse_arguments(int argc, char* argv[], int pdf_count = 3) {
-	if (argc < 8) {
-		throw std::runtime_error("Incorrect usage: " + std::string(argv[0]) + " filename.ext auth1.bmp auth2.bmp ... import1.bmp import2.bmp ... export1.bmp export2.bmp import_dir/ exported_dir/ [lo_previous] [enable_minor_differences]");
-	}
+	if (argc < 10) {
+		throw std::runtime_error("Incorrect usage: " + std::string(argv[0]) + " filename.ext auth1.bmp auth2.bmp ... import1.bmp import2.bmp ... export1.bmp export2.bmp"  +
+		"import_dir/ exported_dir/ import-compare_dir/ export-compare_dir/ [lo_previous] [ms_preivous] [enable_minor_differences]");
 
+	}
 	ParsedArguments args;
 	int arg_index = argc;
 
@@ -47,17 +52,29 @@ ParsedArguments parse_arguments(int argc, char* argv[], int pdf_count = 3) {
 		throw std::runtime_error("Incorrect usage: should either be true of false enabling minor differences");
 	}
 
-	// Deals with [lo_previous]
+	// Deals with [ms_previous]
+	last_arg = argv[arg_index - 1];
+	if (last_arg == "true" || last_arg == "false") {
+		args.ms_previous = (last_arg == "true");
+		arg_index--;
+	} else {
+		throw std::runtime_error("Incorrect usage: should either be true of false for including ms_previous files");
+	}
+
+	// Deals with [ms_preivous]
 	last_arg = argv[arg_index - 1];
 	if (last_arg == "true" || last_arg == "false") {
 		args.lo_previous = (last_arg == "true");
 		arg_index--;
 	} else {
-		throw std::runtime_error("Incorrect usage: should either be true of false for including lo files");
+		throw std::runtime_error("Incorrect usage: should either be true of false for including ms_previous files");
 	}
 
-	args.export_dir = argv[arg_index - 1];
-	args.import_dir = argv[arg_index - 2];
+	args.export_compare_dir = argv[arg_index - 1];
+	args.import_compare_dir = argv[arg_index - 2];
+	args.export_dir = argv[arg_index - 3];
+	args.import_dir = argv[arg_index - 4];
+
 
 	args.basename = argv[1]; // The first argument is the basename of the file, not used in this context
 	size_t dot_pos = args.basename.find_last_of('.');
@@ -66,25 +83,38 @@ ParsedArguments parse_arguments(int argc, char* argv[], int pdf_count = 3) {
 	if (args.lo_previous) {
 		pdf_count++;
 	}
+	if (args.ms_previous) {
+		pdf_count++;
+	}
 
-	int num_image_args = arg_index - 4; // from the count and the program name and basename(filename.doc)
+	int num_image_args = arg_index - 6; // exclude program, basename, import, export, import_compare, export_compare dir
 	if (num_image_args % pdf_count != 0) {
 		throw std::runtime_error("Error: Mismatched number of authoritative and import pages.");
 	}
 
 	int num_pages = num_image_args / pdf_count;
-	for (int i = 0; i < num_pages; i++)
+	for (int i = 0; i < num_pages; i++) {
 		args.authoritative_images.push_back(BMP(argv[i + 2], args.basename)); // Plus one to skip the program name
+	}
 
-	for (int i = 0; i < num_pages; i++)
+	for (int i = 0; i < num_pages; i++) {
 		args.import_images.push_back(BMP(argv[i + 2 + num_pages], args.basename)); // Import pages start after the authoritative pages
+	}
 
-	for (int i = 0; i < num_pages; i++)
+	for (int i = 0; i < num_pages; i++) {
 		args.export_images.push_back(BMP(argv[i + 2 + num_pages * 2], args.basename)); // Export pages start after the import page
+	}
 
 	if (args.lo_previous) {
-		for (int i = 0; i < num_pages; i++)
+		for (int i = 0; i < num_pages; i++) {
 			args.import_previous_images.push_back(BMP(argv[i + 2 + num_pages * 3], args.basename));
+		}
+	}
+
+	if (args.ms_previous) {
+		for (int i = 0; i < num_pages; i++) {
+			args.export_previous_images.push_back(BMP(argv[i + 2 + num_pages * 4], args.basename));
+		}
 	}
 	return args;
 }
@@ -132,25 +162,37 @@ int main(int argc, char* argv[])
 			BMP export_lo = args.export_images[i];
 
 			// Diffing the authoritative page with the imported page
-			std::string import_diff_path = args.import_dir + "/" + args.extension + "/" + args.basename + "_import-page-" + std::to_string(i + 1) + ".bmp";
+			std::string import_diff_path = args.import_dir + "/" + args.basename + "_import-page-" + std::to_string(i + 1) + ".bmp";
 			BMP import_diff_result = pixel_basher.compare_to_bmp(base, imported, args.enable_minor_differences);
 			import_diff_result.write(import_diff_path.c_str());
 
 			// Diffing the authoritative page with the exported page
-			std::string export_output_path = args.export_dir + "/" + args.extension + "/" + args.basename + "_export-page-" + std::to_string(i + 1) + ".bmp";
+			std::string export_output_path = args.export_dir + "/" + args.basename + "_export-page-" + std::to_string(i + 1) + ".bmp";
 			BMP export_diff_result = pixel_basher.compare_to_bmp(base, export_lo, args.enable_minor_differences);
 			export_diff_result.write(export_output_path.c_str());
 
 			if (args.lo_previous) {
 				BMP import_previous = args.import_previous_images[i];
 
-				std::string import_previous_output_path = args.import_dir + "/" + args.extension + "/"  + args.basename + "_prev-import-" + std::to_string(i + 1) + ".bmp";
+				std::string import_previous_output_path = args.import_dir + "/"  + args.basename + "_prev-import-page-" + std::to_string(i + 1) + ".bmp";
 				BMP import_previous_result = pixel_basher.compare_to_bmp(base, import_previous, args.enable_minor_differences);
 				import_previous_result.write(import_previous_output_path.c_str());
 
-				std::string import_previous_comapare_output_path = args.import_dir  + "-compare/" + args.extension + "/" + args.basename + "_import-compare-" + std::to_string(i + 1) + ".bmp";
+				std::string import_previous_comapare_output_path = args.import_compare_dir + "/" + args.basename + "_import-compare-page-" + std::to_string(i + 1) + ".bmp";
 				BMP current_previous_result = pixel_basher.compare_regressions(base, import_diff_result, import_previous_result);
 				current_previous_result.write(import_previous_comapare_output_path.c_str());
+			}
+
+			if (args.ms_previous) {
+				BMP export_previous = args.export_previous_images[i];
+
+				std::string export_previous_output_path = args.export_dir + "/"  + args.basename + "_prev-export-page-" + std::to_string(i + 1) + ".bmp";
+				BMP export_previous_result = pixel_basher.compare_to_bmp(base, export_previous, args.enable_minor_differences);
+				export_previous_result.write(export_previous_output_path.c_str());
+
+				std::string export_previous_compare_output_path = args.export_compare_dir + "/" + args.basename + "_import-compare-page-" + std::to_string(i + 1) + ".bmp";
+				BMP current_previous_result = pixel_basher.compare_regressions(base, export_diff_result, export_previous_result);
+				current_previous_result.write(export_previous_compare_output_path.c_str());
 			}
 
 			// Write stats to CSV
