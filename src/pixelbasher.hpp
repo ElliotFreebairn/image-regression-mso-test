ae717f93df309de3fa50c39920de8589712c3eb7
elliotfreebairn <elliotfreebairn1@gmail.com>
2025-07-31 11:52:25 +0100

    Replacing missed std::arrays with PixelValues
    Signed-off-by: Elliot Freebairn <elliot.freebairn@collabora.com>

--- src/pixelbasher.hpp
+++ src/pixelbasher.hpp
@@ -36,8 +36,8 @@ public:
 
 private:
     static std::vector<bool> get_intersection_mask(const BMP &original, const BMP &target, int min_width, int min_height);
-    static std::array<std::uint8_t, 4> compare_pixel_regression(std::array<std::uint8_t, 4> original, std::array<std::uint8_t, 4> current, std::array<std::uint8_t, 4> previous);
-    static std::array<std::uint8_t, 4> compare_pixels(std::array<std::uint8_t, 4> original, std::array<std::uint8_t, 4> target, BMP &diff, bool near_edge, bool vertical_edge, bool minor_differences);
-    static std::array<std::uint8_t, 4> colour_pixel(Colour colour);
+    static PixelValues compare_pixel_regression(PixelValues original, PixelValues current, PixelValues previous);
+    static PixelValues compare_pixels(PixelValues original, PixelValues target, BMP &diff, bool near_edge, bool vertical_edge, bool minor_differences);
+    static PixelValues colour_pixel(Colour colour);
 };
 #endif
