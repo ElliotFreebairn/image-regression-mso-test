ab25c75cfdf6a18c31e3af6218a601eee2d9879f
elliotfreebairn <elliotfreebairn1@gmail.com>
2025-08-04 16:51:06 +0100

    Hooking up to BMP code
    Had to fix makefile to compile and link the actual source code of bmp and pixelbasher
    
    Also the BMP's i put into source are not 32 so switched out the solid_white image and the tests are now passing
    
    Signed-off-by: Elliot Freebairn <elliot.freebairn@collabora.com>

--- test/test_bmp.cpp
+++ test/test_bmp.cpp
@@ -1,6 +1,9 @@
 #include <catch2/catch.hpp>
 #include "../src/bmp.hpp"
 
+#include <iostream>
+
 TEST_CASE("some test") {
-    REQUIRE(1 + 1 == 2);
+    std::string sw_path = "test_data/solid_white.bmp";
+    BMP solid_white(sw_path.c_str());
 }
