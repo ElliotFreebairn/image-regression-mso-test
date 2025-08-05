a8ef28765be23e5f5f96157e2832193ec3609d8b
elliotfreebairn <elliotfreebairn1@gmail.com>
2025-08-05 11:07:15 +0100

    Better TEST_CASE and SECTION names
    Signed-off-by: Elliot Freebairn <elliot.freebairn@collabora.com>

--- test/test_bmp.cpp
+++ test/test_bmp.cpp
@@ -4,51 +4,51 @@
 #include <bit>
 #include <iostream>
 
-TEST_CASE("BMP's read functionality") {
+TEST_CASE("Read BMP files" , "[bmp][read]") {
     std::string bmp_path;
-    SECTION("opening and reading solid_white.bmp") {
+    SECTION("read solid_white.bmp succesfully") {
         bmp_path = "test_data/solid_white.bmp";
         BMP solid_white;
 
         REQUIRE_NOTHROW(solid_white.read(bmp_path.c_str()));
     }
 
-    SECTION("opening and reading solid_black.bmp") {
+    SECTION("read solid_black.bmp succesfully") {
         bmp_path = "test_data/solid_black.bmp";
         BMP solid_black;
 
         REQUIRE_NOTHROW(solid_black.read(bmp_path.c_str()));
     }
 
-    SECTION("opening and reading white_black.bmp") {
+    SECTION("read white_black.bmp succesfully") {
         bmp_path = "test_data/white_black.bmp";
         BMP black_white;
 
         REQUIRE_NOTHROW(black_white.read(bmp_path.c_str()));
     }
 
-    SECTION("opening and reading vertical_edges.bmp") {
+    SECTION("read vertical_edges.bmp succesfully") {
         bmp_path = "test_data/vertical_edges.bmp";
         BMP vertical_edges;
 
         REQUIRE_NOTHROW(vertical_edges.read(bmp_path.c_str()));
     }
 
-    SECTION("opening and reading edges.bmp") {
+    SECTION("read edges.bmp succesfully") {
         bmp_path = "test_data/edges.bmp";
         BMP edges;
 
         REQUIRE_NOTHROW(edges.read(bmp_path.c_str()));
     }
 
-    SECTION("opening and reading not_bmp.png") {
+    SECTION("throws error when reading non-BMP file") {
         bmp_path = "test_data/not_bmp.png";
         BMP not_bmp;
 
         REQUIRE_THROWS_WITH(not_bmp.read(bmp_path.c_str()), Catch::Contains("Not a BMP") && Catch::Contains("BM"));
     }
 
-    SECTION("opening and reading 24_bit.bmp") {
+    SECTION("throws error when reading 24-bit BMP") {
         bmp_path = "test_data/24_bit.bmp";
         BMP bad_image;
 
@@ -56,16 +56,16 @@ TEST_CASE("BMP's read functionality") {
     }
 }
 
-TEST_CASE("BMP write functionality") {
+TEST_CASE("Writing BMP files", "[bmp][write]") {
     std::string bmp_path;
     bmp_path = "test_data/output/100x100.bmp";
     BMP dummy_image(100, 100);
 
-    SECTION("creating a 100x100 bmp and write it out") {
+    SECTION("creating and writes a 100x100 BMP succesfully") {
         REQUIRE_NOTHROW(dummy_image.write(bmp_path.c_str()));
     }
 
-    SECTION("writing a 100x100 bmp and reading it back in to check equivalence") {
+    SECTION("writes and re-reads 100x100 BMP with matching data") {
         dummy_image.write(bmp_path.c_str());
 
         BMP dummy_image_read(bmp_path.c_str());
@@ -75,29 +75,26 @@ TEST_CASE("BMP write functionality") {
     }
 }
 
-TEST_CASE("Extracting average colour from BMP's") {
+TEST_CASE("Computing average colour in BMP images", "[bmp][pixel-analysis]") {
     std::string bmp_path;
-    SECTION("extracting solid whites average colour") {
+    SECTION("returns 255 for solid_white.bmp") {
         bmp_path = "test_data/solid_white.bmp";
         BMP solid_white (bmp_path.c_str());
 
-        // Solid white, means average colour should be 255
         REQUIRE(solid_white.get_background_value() == 255);
     }
 
-    SECTION("extracting solid black average colour") {
+    SECTION("returns 0 for solid_black.bmp") {
         bmp_path = "test_data/solid_black.bmp";
         BMP solid_black (bmp_path.c_str());
 
-        // Solid black, means average colour should be 0
         REQUIRE(solid_black.get_background_value() == 0);
     }
 
-    SECTION("extracting white_black average colour") {
+    SECTION("returns 127 for white_black.bmp") {
         bmp_path = "test_data/white_black.bmp";
         BMP white_black (bmp_path.c_str());
 
-        // half white and black, means avg is 127
         REQUIRE(white_black.get_background_value() == 127);
     }
 }
